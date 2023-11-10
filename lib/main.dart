39b31488cfbfa2c7f133ed10e7580d318e50e38c
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-11-10 23:10:52 +0300

    iWallet.png rgb

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'iWallet',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor:Color.fromRGBO(2, 54, 113, 1)), // rgb iWallet icon ile aynı.
         useMaterial3: true,
       ),
       home: const UserPage(),
