870f0fb059ea2dc72b40884f3fb2d1f066cfb843
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-11-10 21:06:47 +0300

    Provider ve Http paketleri eklendi. UserPage sayfası olusturuldu..

--- lib/screens/users_page.dart
+++ lib/screens/users_page.dart
@@ -5,10 +5,31 @@ class UserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(),
-      body: Container(),
-
+      body: Column(
+        children: [
+          const Expanded(flex: 1, child: Text("iWallet Users",style: TextStyle(fontSize: 40)),),
+          const Expanded(flex: 1, child: TextField()),
+          Expanded(
+              flex: 5,
+              child: ListView(
+                children: [
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                  Text("Hey",style: TextStyle(fontSize: 50),),
+                ],
+              ))
+        ],
+      ),
     );
   }
 }
