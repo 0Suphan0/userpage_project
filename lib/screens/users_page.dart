f70cbaa8bc719ae5602396f142beef4807469d71
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-11-10 21:55:10 +0300

    UsersPage Sayfası tasarımı tamamlandı..

--- lib/screens/users_page.dart
+++ lib/screens/users_page.dart
@@ -3,32 +3,94 @@ import 'package:flutter/material.dart';
 class UserPage extends StatelessWidget {
   const UserPage({Key? key}) : super(key: key);
 
+  final String projectTitle = "iWallet Users";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      // keyboard acılınca ekran re-size olmasın.
       appBar: AppBar(),
       body: Column(
         children: [
-          const Expanded(flex: 1, child: Text("iWallet Users",style: TextStyle(fontSize: 40)),),
-          const Expanded(flex: 1, child: TextField()),
           Expanded(
-              flex: 5,
-              child: ListView(
-                children: [
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                  Text("Hey",style: TextStyle(fontSize: 50),),
-                ],
-              ))
+              flex: 1,
+              child: Text(projectTitle, style: TextStyle(fontSize: 40))),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                  labelText: 'Kullanıcı Ara',
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black26),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                child: ListView(
+                  children: [
+                    buildUserCard(
+                        "Ahmet Yılmaz", "@ahmet", Icons.chevron_right),
+                    buildUserCard(
+                        "Ahmet Yılmaz", "@ahmet", Icons.chevron_right),
+                    buildUserCard(
+                        "Ahmet Yılmaz", "@ahmet", Icons.chevron_right),
+                    buildUserCard(
+                        "Ahmet Yılmaz", "@ahmet", Icons.chevron_right),
+                    buildUserCard(
+                        "Ahmet Yılmaz", "@ahmet", Icons.chevron_right),
+                    buildUserCard(
+                        "Ahmet Yılmaz", "@ahmet", Icons.chevron_right),
+                    buildUserCard(
+                        "Ahmet Yılmaz", "@ahmet", Icons.chevron_right),
+                    buildUserCard(
+                        "Ahmet Yılmaz", "@ahmet", Icons.chevron_right),
+                    buildUserCard(
+                        "Ahmet Yılmaz", "@ahmet", Icons.chevron_right),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget buildUserCard(String name, String username, IconData iconData) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4), // sağdan soldan 4
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius:
+              BorderRadius.circular(30.0), // circular kenarlar 30 set edildi.
+        ),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Icon(Icons.person),
+          ),
+          title: Text(name, style: TextStyle(fontSize: 20)),
+          subtitle: Text(username, style: TextStyle(fontSize: 16)),
+          trailing: Icon(iconData),
+          onTap: () {},
+        ),
       ),
     );
   }
